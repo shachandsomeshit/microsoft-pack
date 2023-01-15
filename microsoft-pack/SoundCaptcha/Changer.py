cfc9be5e41e057af583b0240dfa1c7ffefea636d
shachandsomeshit <99337753+shachandsomeshit@users.noreply.github.com>
2023-01-15 22:15:07 +0200

    Delete microsoft-pack directory

--- microsoft-pack/SoundCaptcha/Changer.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog
-
-input_ = input("text:")
-orginal_site_key = input("orginal key:")
-site_key = input("key:")
-
-Token = input_.replace(orginal_site_key, site_key)
-print(Token)
